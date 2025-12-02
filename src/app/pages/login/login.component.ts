dd5181832c5aa15d0da2f2297dcc952e269b0e4c
RAISSA MARA ALVES GOMES SANTANA <020.720092@universidadesenaicimatec.edu.br>
2025-12-02 15:57:46 -0300

    Login
    coreção da ausencia dos elementos

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+        // Objeto para armazenar os dados do formulário
+  loginData = {
+    email: '',
+    password: '',
+    agreeToTerms: false
+  };
 
+  onSubmit() {
+    if (this.loginData.agreeToTerms) {
+      console.log('Tentando logar com:', this.loginData);
+      // Aqui você colocará a chamada para o seu AuthService
+    } else {
+      alert('Por favor, aceite os termos para continuar.');
+    }
+  }
 }

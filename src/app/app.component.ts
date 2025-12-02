085b5fe43c935aa5acae399fa91b4923bdefd3a6
RAISSA MARA ALVES GOMES SANTANA <020.720092@universidadesenaicimatec.edu.br>
2025-12-02 15:45:40 -0300

    base do Projeto
    header, footer,home, login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./componentes/header/header.component";
+import { FooterComponent } from "./componentes/footer/footer.component";
+import { LoginComponent } from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
